34b9d397e228f1b7c9f50e365b286253c3a25868
nsadisha <nsadisha@gmail.com>
2022-04-12 02:00:05 +0530

    custom queries

--- src/main/java/com/kln/lms/api/repository/CourseRepository.java
+++ src/main/java/com/kln/lms/api/repository/CourseRepository.java
@@ -1,7 +1,13 @@
 package com.kln.lms.api.repository;
 
 import com.kln.lms.api.model.Course;
+import com.kln.lms.api.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Integer> {
+    @Query("SELECT c.enrolledStudents FROM Course c WHERE c.course_id = ?1")
+    List<Student> getEnrolledStudents(Integer courseId);
 }
