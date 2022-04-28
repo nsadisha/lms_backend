cb631b30c4625f754168f1272a368be22a73fc07
nsadisha <nsadisha@gmail.com>
2022-04-28 12:49:55 +0530

    changed primary key type

--- src/main/java/com/kln/lms/api/repository/LecturerRepository.java
+++ src/main/java/com/kln/lms/api/repository/LecturerRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LecturerRepository extends JpaRepository<Lecturer, String> {
+public interface LecturerRepository extends JpaRepository<Lecturer, Integer> {
     Lecturer findLecturerByEmail(String email);
 }
