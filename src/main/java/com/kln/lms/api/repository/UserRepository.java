ae62d806b8a25b652250108528544a20ac2e7989
nsadisha <nsadisha@gmail.com>
2022-04-28 12:48:00 +0530

    changed primary key type

--- src/main/java/com/kln/lms/api/repository/UserRepository.java
+++ src/main/java/com/kln/lms/api/repository/UserRepository.java
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findUserByEmail(String email);
-    User findUserByIdAndRoleEquals(Integer id, String role);
 }
