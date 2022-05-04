75bf3a09ebf5487af8de9ca161d942ace34204e1
tchamalsha <tharu.chamalsha@gmail.com>
2022-05-04 22:25:08 +0530

    add api

--- src/main/java/com/kln/lms/api/emails/MailGunEmailService.java
+++ src/main/java/com/kln/lms/api/emails/MailGunEmailService.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class MailGunEmailService implements EmailService{
 
     private static final String DOMAIN_NAME = "sandbox73944287eef348c7a66b5898cd5cf8e6.mailgun.org";
-
+    private static final String API_KEY = "142c96fb3ed425a980b32b05884b41cf-fe066263-1f6c211c";
     @Override
     public void sendEmail(EmailModel email) throws IOException {
 
