04712dafb2f6bce1ea2b68ddfb84ef4a7df40220
Hasini <hasinisama99@gmail.com>
2022-05-12 15:41:50 +0530

    Added more data to db

--- src/main/java/com/kln/lms/api/LmsBackendApplication.java
+++ src/main/java/com/kln/lms/api/LmsBackendApplication.java
@@ -1,8 +1,6 @@
 package com.kln.lms.api;
 
-import com.kln.lms.api.model.Course;
-import com.kln.lms.api.model.Lecturer;
-import com.kln.lms.api.model.Student;
+import com.kln.lms.api.model.*;
 import com.kln.lms.api.service.CourseServiceImpl;
 import com.kln.lms.api.service.LecturerServiceImpl;
 import com.kln.lms.api.service.StudentServiceImpl;
@@ -32,17 +30,18 @@ public class LmsBackendApplication {
 
 	@Bean
 	CommandLineRunner run(StudentServiceImpl studentService, UserServiceImpl userService,
-						  CourseServiceImpl courseService){
+						  CourseServiceImpl courseService, LecturerServiceImpl lecturerService){
 		return args -> {
 
-			userService.saveUser(new Student(null, "Hasini", "Hasini@gmail.com", "123", "STUDENT"));
-			userService.saveUser(new Student(null, "Sadisha", "Sadisha@gmail.com", "123", "STUDENT"));
-			userService.saveUser(new Student(null, "Tharushi", "Tharushi@gmail.com", "123", "STUDENT"));
+			userService.saveUser(new User(null, "Hasini Kavisha", "Hasini@gmail.com", "123", "STUDENT"));
+			userService.saveUser(new User(null, "Sadisha Nimsara", "Sadisha@gmail.com", "123", "STUDENT"));
+			userService.saveUser(new User(null, "Tharushi Chamalsha", "Tharushi@gmail.com", "123", "STUDENT"));
 
-			userService.saveUser(new Lecturer(null, "Amara", "Amara@gmail.com", "123", "LECTURER"));
-			userService.saveUser(new Lecturer(null, "Kasun", "Kasun@gmail.com", "123", "LECTURER"));
+			userService.saveUser(new User(null, "Ms. Amara Samarasignhe", "Amara@gmail.com", "123", "LECTURER"));
+			userService.saveUser(new User(null, "Mr. Kasun Rukmaldeniya", "Kasun@gmail.com", "123", "LECTURER"));
+			userService.saveUser(new User(null, "Ms. Maheshi Perera", "Maheshi@gmail.com", "123", "LECTURER"));
 
-			userService.saveUser(new Lecturer(null, "Admin", "admin@admin.com", "123", "ADMIN"));
+			userService.saveUser(new User(null, "Admin", "admin@admin.com", "123", "ADMIN"));
 
 			courseService.saveCourse(new Course(null, "SENG 22212", "Software Architecture", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", null, new HashSet<>(), new HashSet<>()), "Amara@gmail.com");
 			courseService.saveCourse(new Course(null, "SENG 12243", "Software Construction", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", null, new HashSet<>(), new HashSet<>()), "Amara@gmail.com");
@@ -53,6 +52,11 @@ public class LmsBackendApplication {
 			studentService.addStudentToCourse(1,2);
 			studentService.addStudentToCourse(2,1);
 			studentService.addStudentToCourse(3,4);
+
+			lecturerService.postAnnouncement(1, new Announcement(null,"Assignment Deadline Postponed", "The deadline for the final assignment of the course module has been postponed due the prevailing situation in the country", null));
+			lecturerService.postAnnouncement(3, new Announcement(null,"New Assignment Added", "New assignment has been added to the course module. The deadline is tomorrow", null));
+			lecturerService.postAnnouncement(3, new Announcement(null,"New Assignment Added", "New assignment has been added to the course module. The deadline is today", null));
+			lecturerService.postAnnouncement(4, new Announcement(null,"Assignment Deadline Postponed", "The deadline for the final assignment of the course module has been postponed due the prevailing situation in the country", null));
 
 		};
 	}
--- src/main/java/com/kln/lms/api/security/SecurityConfig.java
+++ src/main/java/com/kln/lms/api/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.kln.lms.api.security;
 
 import com.kln.lms.api.filter.CustomAuthenticationFilter;
 import com.kln.lms.api.filter.CustomAuthorizationFilter;
+import com.kln.lms.api.service.CourseServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -28,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-//    private final CourseServiceImpl courseService;
+    private final CourseServiceImpl courseService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -52,6 +54,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         //permissions
         http.authorizeRequests().antMatchers("/student/**").hasAnyAuthority("STUDENT", "ADMIN");
+
+//        courseService.getAll().forEach(course -> {
+//            try {
+//                http.authorizeRequests().antMatchers("/course/"+course.getCourse_id()).hasAuthority(course.getCourse_code());
+//            } catch (Exception exception) {
+//                exception.printStackTrace();
+//            }
+//        });
+
         http.authorizeRequests().antMatchers("/lecturer/**").hasAnyAuthority("LECTURER", "ADMIN");
         http.authorizeRequests().antMatchers("/admin/**").hasAuthority("ADMIN");
 
