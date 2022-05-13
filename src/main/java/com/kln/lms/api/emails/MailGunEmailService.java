46ab7d40498a996a1d1753903b4955e076f2d5f0
tchamalsha <tharu.chamalsha@gmail.com>
2022-05-13 15:13:44 +0530

    change from email

--- src/main/java/com/kln/lms/api/emails/MailGunEmailService.java
+++ src/main/java/com/kln/lms/api/emails/MailGunEmailService.java
@@ -12,12 +12,13 @@ public class MailGunEmailService implements EmailService{
 
     private static final String DOMAIN_NAME = "sandbox73944287eef348c7a66b5898cd5cf8e6.mailgun.org";
     private static final String API_KEY = "142c96fb3ed425a980b32b05884b41cf-fe066263-1f6c211c";
+
     @Override
     public void sendEmail(EmailModel email) throws IOException {
 
         HttpResponse<JsonNode> request = Unirest.post("https://api.mailgun.net/v3/" + DOMAIN_NAME + "/messages")
                 .basicAuth("api", API_KEY)
-                .field("from", "Excited User <USER@YOURDOMAIN.COM>")
+                .field("from", "lms@lms.com")
                 .field("to", email.getReceiver())
                 .field("subject", email.getSubject())
                 .field("text", email.getBody())
@@ -25,4 +26,6 @@ public class MailGunEmailService implements EmailService{
 
 
     }
+
+
 }
