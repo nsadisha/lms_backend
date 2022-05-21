27628a0b3a587be59c2e0a4ebfe139b396feed7c
Hasini <hasinisama99@gmail.com>
2022-05-21 16:09:41 +0530

    Updated security access

--- src/main/java/com/kln/lms/api/security/SecurityConfig.java
+++ src/main/java/com/kln/lms/api/security/SecurityConfig.java
@@ -40,7 +40,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/signup").anonymous();
 
         //permissions
-//        http.authorizeRequests().antMatchers("/student/{id}/course/{id}").hasAuthority("LECTURER");
+        http.authorizeRequests().antMatchers("/refresh_token").permitAll();
+
+        //http.authorizeRequests().antMatchers("/student/{id}/course/{id}").hasAuthority("LECTURER");
         http.authorizeRequests().antMatchers("/student/**").hasAnyAuthority("STUDENT", "ADMIN");
 
         http.authorizeRequests().antMatchers("/lecturer/**").hasAnyAuthority("LECTURER", "ADMIN");
